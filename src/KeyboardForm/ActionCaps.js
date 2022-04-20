a69bc62b1129784b2eb9d0c571832581d4a86b2e
init.defaultBranch <benlimpic@gmail.com>
2022-04-20 15:19:28 -0700

    Added unique color names to all keyboard form options

--- src/KeyboardForm/ActionCaps.js
+++ src/KeyboardForm/ActionCaps.js
@@ -6,11 +6,17 @@ export default function ActionCaps() {
         <h4>ActionCaps</h4>
         <select className="dropDown">
             <option>Select A Color</option>
-            <option>PALE TEAL</option>
-            <option>MAUVEY NUDE</option>
-            <option>DARKEST GRAPE</option>
-            <option>DONKEY KONG</option>
-            <option>ALPINE HERBS</option>
+            <option>EGYPTIAN BLUE</option>
+            <option>VAMPIRE HUNTER</option>
+            <option>TOY CAMOUFLAGE</option>
+            <option>ICE CLIMBER</option>
+            <option>YOSHI GREEN</option>
+            <option>MILD MENTHOL</option>
+            <option>RETRO NECTARINE</option>
+            <option>DEATH OF A STAR</option>
+            <option>MAXIMUM YELLOW</option>
+            <option>DR. WHITE</option>
+
         </select>
     </div>
   )
