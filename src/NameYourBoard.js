891d8a7e21c0f9428966f8992d5b1f7d581a30b6
init.defaultBranch <benlimpic@gmail.com>
2022-04-21 22:59:10 -0700

    VICTORY

--- src/NameYourBoard.js
+++ src/NameYourBoard.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import {useState} from 'react'
 
 export default function NameYourBoard() {
+const [name, setName] = useState("")
+const [getId, setGetId] = useState(0)
+const handleName = (e) => {
+  e.preventDefault()
+  setName(e.target.value)
+}
+
+const handlePatch = (e) => {
+  e.preventDefault()
+  e.target.reset()
+
+  fetch("http://localhost:3000/saved")
+  .then(r => r.json())
+  .then(setGetId)
+
+  const arrayLength = getId.length
+
+  console.log(arrayLength)
+
+  fetch(`http://localhost:3000/saved/${arrayLength}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({"name": name})
+  })
+
+
+
+}
 
   return (
       <>
         <h1>Name Your Board</h1>
         <h5>Name</h5>
-        <input className="input" value="Name" type="text"></input>
-        <button id='Name-save'>Save</button>
+        <form onSubmit={handlePatch}>
+        <input className="input"  type="text" onChange={handleName}></input>
+        <button id='Name-save' >Save</button>
+        </form>
+        
       </>
   )
 }
